ae35bd1cc25f14fbf19c186b9bec9675a9a34757
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-04 18:15:02 +0530

    happy code

---
+++ happy.java
@@ -0,0 +1,18 @@
+public class happy
+{
+    public static void main(String[] args) {
+        System.out.println("start program");
+        int a=5;
+        int b=0;
+        int c=a/b;
+        System.out.println("c="+c);
+        System.out.println("program end");
+
+        int d=0;
+        int e=a/d;
+        System.out.println(e);
+        System.out.println("end");
+
+//        String Name="alok";
+    }
+}
