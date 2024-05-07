29dacce4c680ca0a691d6823d0c5c5712ceefcc5
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-07 23:22:00 +0530

    special number

---
+++ special_number.java
@@ -0,0 +1,39 @@
+//special two digits number
+import java.util.*;
+public class special_number
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter any number");
+        int n=sc.nextInt();//taking user input
+        int k=n;//k will store value of n
+        int sum=0,product=1,rem;
+        while(n!=0)
+        {
+            rem=n%10;
+            n=n/10;
+            sum=sum+rem;
+            product=product*rem;
+        }
+        System.out.println("this is sum of input number "+sum);
+        System.out.println("this is product of input number "+product);
+
+        //now adding sum and product
+
+        int match=sum+product;
+        System.out.println("this is the sum of sum and product "+match);
+
+        //matching is input number is equal to match
+        System.out.println();
+        System.out.println("________________________________");
+        if(match==k)
+        {
+            System.out.println("Yes "+k+" is special number");
+        }
+        else
+        {
+            System.out.println("No "+k+" is not special number");
+        }
+    }
+}
