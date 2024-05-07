8683e62711a85d622eb100c84c7085eb215f0620
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-07 23:26:08 +0530

    food order program

---
+++ Testfoodorder.java
@@ -0,0 +1,9 @@
+public class Testfoodorder
+{
+    public static void main(String[] args) {
+        foodorder fo=new foodorder();
+        fo.displaymenu();
+        fo.order();
+        fo.generatebill();
+    }
+}
