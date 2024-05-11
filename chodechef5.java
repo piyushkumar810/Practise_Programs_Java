6c469fc425a7e869d5ab41e944f2dfb53c2762e0
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-05-11 23:48:39 +0530

    codechief_problem_5

---
+++ chodechef5.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+class Codechef5
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int i = 1; i <= T; i++)
+        {
+            int x=sc.nextInt();
+            int s=x*15;
+            System.out.println(+s);
+        }
+    }
+}
+
